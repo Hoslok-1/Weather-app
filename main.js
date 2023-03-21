67b31088e54c24392159487e63f36f2b4d773efa
Hoslok-1 <anujhoslokdev@gmail.com>
2023-03-21 14:24:40 +0530

    files setup

---
+++ main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log("Hello");
+let age = 99;
+console.log(age);
+
+/******/ })()
+;
